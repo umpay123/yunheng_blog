142227b4d3b590815a3b5b4ab01404253287a64c
tanlin41 <tanlin41@jd.com>
2022-06-09 11:15:25 +0800

    提交github

--- vue_hy_web/config/dev.env.js
+++ vue_hy_web/config/dev.env.js
@@ -5,10 +5,10 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
 
-  VUE_HY_WEB: '"http://124.70.200.174:9527"',
-  PICTURE_API: '"http://124.70.200.174:8602"',
-  WEB_API: '"http://124.70.200.174:8603"',
-  Search_API: '"http://124.70.200.174:8605"',
-	ELASTICSEARCH: '"http://124.70.200.174:8605"',
+  VUE_HY_WEB: '"http://localhost:9527"',
+  PICTURE_API: '"http://localhost:8602"',
+  WEB_API: '"http://localhost:8603"',
+  Search_API: '"http://localhost:8605"',
+	ELASTICSEARCH: '"http://localhost:8605"',
 
 })
